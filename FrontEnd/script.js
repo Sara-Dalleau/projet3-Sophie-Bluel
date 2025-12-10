67fef049bb43d522201cc3521e947fa467a01f11
Dalleau Sara <sara.dalleau@gmail.com>
2025-12-10 15:21:26 +0100

    finalisation function recupererTravaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,10 +2,20 @@
 async function  recupererTravaux() {
   const reponse = await fetch("http://localhost:5678/api/works");
   const travaux  = await reponse.json();
+  console.log("voici mes travaux", travaux);
+
   for (let i = 0; i < travaux.length; i++) {
-    //creer balises img et title pour afficher les travaux + appendchild + ajout
-    // class a recuperer gallery
+    const sectionTravaux = document.querySelector(".gallery");
+    const imgElement = document.createElement("img");
+    const titleElement = document.createElement("h3");
+    const figureElement = document.createElement("figure");
+
+    imgElement.src = travaux[i].imageUrl;
+    titleElement.textContent = travaux[i].title;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(titleElement);
+    sectionTravaux.appendChild(figureElement);
   }
 }
-// await (recupererTravaux())
-
+const appelRecupererTravaux = recupererTravaux();

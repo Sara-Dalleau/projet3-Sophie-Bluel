d73122707b447d14a8a087599eb2c52143d42d02
Dalleau Sara <sara.dalleau@gmail.com>
2025-12-10 19:03:04 +0100

    ajout filtres dynamiques + affichage des travaux filtrés

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,53 +1,89 @@
 // Creation variable globale pour réutiliser le tableau travaux 
 let tableauElement = [];
 // Fonction asynchrone qui récupère les travaux depuis l'API
-async function  recupererTravaux() {
-  // Appel à l'API pour récupérer les travaux
+async function recupererTravaux() {
   const reponse = await fetch("http://localhost:5678/api/works");
-  // Transformation de la réponse en tableau d'objets JavaScript
-  const travaux  = await reponse.json();
+  const travaux = await reponse.json();
 
-  // Vérification dans la console que les données sont bien récupérées
-  //console.log("voici mes travaux", travaux);
-
-  // Boucle qui parcourt chaque travail du tableau
-  for (let i = 0; i < travaux.length; i++) {
-  // Sélection de la galerie dans le DOM (parent)
-  const sectionTravaux = document.querySelector(".gallery")
-  // Création des éléments HTML
-    const imgElement = document.createElement("img");
-    const titleElement = document.createElement("h3");
-    const figureElement = document.createElement("figure");
-
-  // Insertion de l'image et du titre dans les éléments créés
-    imgElement.src = travaux[i].imageUrl;
-    titleElement.textContent = travaux[i].title;
-
-  // Ajout de l'image et du titre dans la balise figure, puis ajout de la figure complète dans la galerie
-    figureElement.appendChild(imgElement);
-    figureElement.appendChild(titleElement);
-    sectionTravaux.appendChild(figureElement);
-  }
+  tableauElement = travaux;
 }
-//Appel de la fonction
-const appelRecupererTravaux = recupererTravaux();
 
 //Fonction qui récupère les filtres 
 async function recupererFiltres () {
   const reponse = await fetch ("http://localhost:5678/api/categories");
   const filtres = await reponse.json();
   //console.log("voici mes filtres", filtres)
-  const sectionFiltres = document.querySelector(".filtres")
+  const sectionFiltres = document.querySelector(".filtres");
 
   const btnTous = document.createElement("button");
-  btnTous.textContent = ("Tous")
+  btnTous.textContent = ("Tous");
   sectionFiltres.appendChild(btnTous);
+  btnTous.dataset.id = 0;
 
   for (let i = 0; i < filtres.length; i++) {
     const btnElement = document.createElement("button");
     btnElement.textContent = filtres[i].name;
+    // mettre un data-id sur un bouton
+    btnElement.dataset.id = filtres[i].id;
     sectionFiltres.appendChild(btnElement);
   }
 }
-const appelRecupererFiltres = recupererFiltres();
 
+// Fonction pour rendre mes filtres interactifs
+function btnFiltres () {
+  // Sélection de tous les boutons présents dans la section filtres
+  const boutonFiltres = document.querySelectorAll(".filtres button");
+  // Boucle pour ajouter un événement "click" sur chaque bouton
+  for (let i = 0; i < boutonFiltres.length; i++){
+    boutonFiltres[i].addEventListener("click", function (event) {
+      // Récupération de l'id du bouton cliqué (converti en nombre)
+      const btnClique = parseInt(event.target.dataset.id);
+      // Si le bouton "Tous" est cliqué (id = 0), on réaffiche tous les travaux
+      if (btnClique === 0) {
+        afficherTravaux(tableauElement);
+    } 
+    // Sinon, on filtre les travaux selon l'id de la catégorie cliquée
+    else {
+        const travauxFiltres = tableauElement.filter(function (travail) {
+          return travail.categoryId === btnClique;
+      });
+      // Affichage des travaux filtrés
+      afficherTravaux(travauxFiltres);
+    }
+    })
+  }  
+}
+// Fonction qui permet d'afficher dynamiquement les travaux dans la galerie
+function afficherTravaux(tableau) {
+// Récupère la section HTML qui contient la galerie  
+  const sectionTravaux = document.querySelector(".gallery");
+  // Vide la galerie avant d'afficher de nouveaux éléments
+  sectionTravaux.innerHTML = "";
+
+  for (let i = 0; i < tableau.length; i++) {
+    // Création de la balise figure qui va contenir l'image et le titre
+    const figureElement = document.createElement("figure");
+    // Création de la balise <img> pour l'image du travail
+    const imgElement = document.createElement("img");
+    // Récupération de l’URL de l’image
+    imgElement.src = tableau[i].imageUrl;
+
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = tableau[i].title;
+    // Ajout de l'image et du titre dans la balise figure
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(titleElement);
+    // Ajout de la figure complète dans la galerie
+    sectionTravaux.appendChild(figureElement);
+  }
+}
+
+//Fonction d'initialisation: attend que les travaux et filtres soient récupérés et affichés, puis active les event sur boutons filtres
+async function init () {
+  await recupererTravaux();
+  afficherTravaux(tableauElement);
+
+  await recupererFiltres();
+  btnFiltres();
+}
+init(); 

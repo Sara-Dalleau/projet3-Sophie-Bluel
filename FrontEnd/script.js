17b058c716545e83385931b91232d2d68e1fa330
Dalleau Sara <sara.dalleau@gmail.com>
2025-12-12 18:39:04 +0100

    mise en place authentification utilisateur formulaire

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -58,6 +58,7 @@ function btnFiltres () {
     })
   }  
 }
+
 // Fonction qui permet d'afficher dynamiquement les travaux dans la galerie
 function afficherTravaux(tableau) {
 // Récupère la section HTML qui contient la galerie  
@@ -83,12 +84,54 @@ function afficherTravaux(tableau) {
   }
 }
 
+//FORMULAIRE 
+  async function formulaire() {
+    // Sélection du formulaire
+  const recupForm = document.querySelector(".formulaire");
+
+  // Écoute de l’événement submit
+  recupForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("submit ok");
+    // Récupération des valeurs email / password
+    const inputEmail = document.getElementById("email").value;
+    const inputPassword = document.getElementById("password").value;
+    // Envoi d’une requête POST /users/login
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      // Body en JSON
+      body: JSON.stringify({ email: inputEmail, password: inputPassword }),
+    });
+    // Analyse de la réponse :
+    const status = reponse.status;
+    // si ok → récupérer le token
+    if (status === 200) {
+      const data = await reponse.json();
+      const token = data.token;
+      // stocker le token dans localStorage
+      localStorage.setItem("token", token);
+      // redirection vers l’accueil
+      window.location.href = "index.html";
+    } else { // si erreur → afficher / mettre à jour un message d’erreur dans le DOM
+      const sectionErreur = document.querySelector(".msgErreur");
+      sectionErreur.innerHTML = "";
+
+      const msgErreur = document.createElement("p");
+      msgErreur.textContent = "E-mail ou mot de passe incorrect";
+      
+      sectionErreur.appendChild(msgErreur);
+    }
+  });
+  }
+  formulaire ();
+
 //Fonction d'initialisation: attend que les travaux et filtres soient récupérés et affichés, puis active les event sur boutons filtres
 async function init () {
   await recupererTravaux();
   afficherTravaux(tableauElement);
 
   await recupererFiltres();
-  btnFiltres();
+  btnFiltres()
 }
 init(); 

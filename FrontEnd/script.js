20bb692fee3f0dd6e25840de8536273b2d2712b6
Dalleau Sara <sara.dalleau@gmail.com>
2025-12-15 17:30:04 +0100

    algorithme modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -134,11 +134,9 @@ init();
     }
   });
   }
-  
   formulaire ();
   
   // FONCTION MODE EDITION
-
   function modeEdition () {
   const etat = localStorage.getItem("token");
 
@@ -146,6 +144,7 @@ init();
     //filtres cachés
     const cacherFiltres = document.querySelector(".filtres");
     cacherFiltres.style.display = "none";
+
     // Login devient logout
     let btnLogin = document.querySelector(".btnLogin");
     btnLogin.setAttribute("href", "#");
@@ -155,6 +154,7 @@ init();
       localStorage.removeItem("token"); //suppression du token
       (window.location.href = "index.html") // redirection page d'accueil
     })
+
     //Création Bouton modifier
     const btnModifier = document.querySelector(".btnModifier");
 
@@ -177,3 +177,87 @@ init();
   }
 }
 modeEdition();
+
+//MODALE
+
+//RAISONNEMENT
+
+// Le click sur le bouton modifier doit déclencher l'ouverte de la modale
+// La modale doit se fermer au click sur la croix + click en dehors de la modale
+// Il y a deux contenue donc deux états différent de la modale
+// Etat : galerie photo + ajout d'une photo via formulaire
+// Au click sur le bouton modifier l'état de la modale est galerie photo
+// Quand je passe de l'état galerie photo a ajout photo la modale,
+// ne doit pas se fermer et juste changer son contenu.
+// Pour passer de galere photo à ajout photo afficher/masquer les zones dans la meme modale
+// Au click retour via ajout photo on revient vers galerie photo dans la meme modale
+// A la fermeture de la modale on reinitialise l'état interne
+
+// Gestion HTML : la modale elle meme, le titre galerie photo, 
+// le bouton ajouter une photo, le bouton croix, le formulaire, 
+// titre ajout photo, la flèche retour
+
+// Gestion CSS: display none sur la modale, le design generale
+
+// Gestion JavaScript : suppression travaux au click icone poubelle,
+// bouton ajouter une photo qui mène vers l'état ajout photo, 
+// ajout de travaux via formulaire, modale qui se ferme au click croix ,
+// ou a coter, fleche qui mene vers galerie photo au click,
+// ouverture modale au click bouton modifier sur la page d'accueil, 
+// savoir si token existe ou non pour accèder à la modale. 
+
+// CODAGE DE LA MODALE
+
+// 1. verifier token 
+
+// HTML
+// 2. Créer modale en HTML: après le header, section modal (conteneur globale), 
+// > dedans un overlay (modal-overlay) > dedans boite de contenu (modal-content).
+
+// 3. a l'interieur de modal-content (bouton croix, 
+// bouton flèche(caché état galerie photo/afficher ajout photo), 
+// titre qui change celon état(ou caché puis affiché)).
+
+// 4. Créer état galerie photo: conteneur pour mini galerie + bouton ajouter une photo
+// puis état ajout photo: conteneur form, input, bouton valider
+
+//CSS 
+// 5. Caché modale par defaut display none
+// 6. overlay : plein écran, fond sombre voir figma, derriere la modale
+// 7. positionner modale: centré, fond blanc, arrondis, padding, z-index au dessus overlay
+// 8. afficher/masquer : zone galerie, zone formulaire, flèche retour
+
+//JS 
+
+// 9. Récupérer dans var: bouton modifier, modale, overlay, boite modale, 
+// croix, flèche retour, zone galerie, zone form, btn ajouter photo .. 
+// 10. Creer variable "état modale", galerie et/ou form 
+// 11. Ouverture modale sans duplication: fonction (rendre la modale visible, 
+// mettre l'état interne à galerie, afficher zone galerie, cacher zone formulaire, cacher flèche retour)
+// 12. addEventListener btn modifier au click ouvrir. test ... 
+// 13. Fermeture modale: fonction closeModal( cacher modale, 
+// reinitialiser état inteerne à galerie, remettre l'affichage par défaut(galerie visible/form caché))
+// 14. addEventListener sur croix et overlay au click fermer modale.
+// 15. verifier click dans conteneur modal-content ne ferme pas la modale. 
+// 16. Aller au form: addEventListener sur btn ajouter photo au click: 
+// changer létat à formulaire(cacher zone galere, afficher zone form, 
+// affiche flèche retour). 
+// 17. retour à la galerie: addEventListener au click: changer l'état 
+// à galerie( afficher zone galerie, cacher zone form, cacher flèche retour).
+// 18. quand ouverture modale en mode galerie : remplir le conteneur avec les travaux.
+// Avant de remplir: vider le contenueur pour eviter doublons.
+// Pour chaque travaux: afficher miniature, ajout icone poubelle cliquable.
+// 19. suppression travaux: au click icone  poubelle; recuperer id du 
+// travail, appeler api de sup, si ok supprimer visuellement 
+// puis maj galerie principale. 
+
+
+// Avant d’afficher > vider
+// Avant de créer > vérifier que ça n’existe pas déjà
+// A la fermeture > réinitialises l’état
+
+
+
+
+
+
